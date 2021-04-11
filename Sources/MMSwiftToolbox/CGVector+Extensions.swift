61036bad54b7948208c905c860034d6284b7fe25
Adam Wulf <adam.wulf@gmail.com>
2021-04-11 17:31:30 -0500

    Adding vector methods

---
+++ Sources/MMSwiftToolbox/CGVector+Extensions.swift
@@ -0,0 +1,43 @@
+//
+//  File.swift
+//  
+//
+//  Created by Adam Wulf on 4/11/21.
+//
+
+import CoreGraphics
+
+extension CGVector {
+
+    public var normalized: CGVector {
+        return normalize()
+    }
+
+    public var magnitude: CGFloat {
+        return sqrt(dx * dx + dy * dy)
+    }
+
+    public var theta: CGFloat {
+        // divide by zero is fine here, as atan handles ±inf properly
+        var theta = atan(dy / dx)
+        let isYNeg = dy < 0
+        let isXNeg = dx < 0
+
+        // adjust the angle depending on which quadrant it's in
+        if isYNeg && isXNeg {
+            theta -= CGFloat.pi
+        } else if !isYNeg && isXNeg {
+            theta += CGFloat.pi
+        }
+        return theta
+    }
+
+    public init(theta: CGFloat) {
+        self.init(dx: cos(theta), dy: sin(theta))
+    }
+
+    public func normalize(to target: CGFloat = 1) -> CGVector {
+        let len = magnitude
+        return CGVector(dx: dx / len * target, dy: dy / len * target)
+    }
+}
--- Tests/MMSwiftToolboxTests/MMSwiftToolboxTests.swift
+++ Tests/MMSwiftToolboxTests/MMSwiftToolboxTests.swift
@@ -17,16 +17,28 @@ final class MMSwiftToolboxTests: XCTestCase {
                        CGRect(x: -2, y: -2, width: 14, height: 14))
     }
 
-    func testCGPoint() {
+    func testCGPointDiff() {
         let p1 = CGPoint(x: 1, y: 2)
         let p2 = CGPoint(x: 4, y: 3)
         let vec = p2 - p1
         XCTAssertEqual(vec, CGVector(dx: 3, dy: 1))
     }
 
+    func testCGVectorTheta() {
+        XCTAssertEqual(CGVector(dx: 1, dy: 0).theta, 0)
+        XCTAssertEqual(CGVector(dx: 0, dy: 1).theta, CGFloat.pi / 2)
+        XCTAssertEqual(CGVector(dx: 0, dy: -1).theta, -CGFloat.pi / 2)
+        XCTAssertEqual(CGVector(dx: -1, dy: 0).theta, CGFloat.pi)
+    }
+
+    func testCGVectorMag() {
+        XCTAssertEqual(CGVector(dx: 1, dy: 0).magnitude, 1)
+        XCTAssertEqual(CGVector(dx: 1, dy: 1).magnitude, sqrt(2))
+    }
+
     static var allTests = [
         ("testExample", testExample),
         ("testCGRect", testCGRect),
-        ("testCGPoint", testCGPoint)
+        ("testCGPointDiff", testCGPointDiff)
     ]
 }
