ab82d7be0d481fb0b480eef6f727be4aa0e57ff3
Adam Wulf <adam.wulf@gmail.com>
2022-05-08 14:58:00 -0500

    rect methods for translating / expanding

--- Sources/SwiftToolbox/CGRect+Extensions.swift
+++ Sources/SwiftToolbox/CGRect+Extensions.swift
@@ -23,4 +23,32 @@ extension CGRect {
     public func expand(by delta: CGFloat) -> CGRect {
         return inset(by: -delta)
     }
+
+    // MARK: - Translate
+
+    static public func - (lhs: CGRect, rhs: CGPoint) -> CGRect {
+        return CGRect(x: lhs.origin.x - rhs.x, y: lhs.origin.y - rhs.y, width: lhs.width, height: lhs.height)
+    }
+
+    static public func + (lhs: CGRect, rhs: CGPoint) -> CGRect {
+        return CGRect(x: lhs.origin.x + rhs.x, y: lhs.origin.y + rhs.y, width: lhs.width, height: lhs.height)
+    }
+
+    static public func - (lhs: CGRect, rhs: CGVector) -> CGRect {
+        return CGRect(x: lhs.origin.x - rhs.dx, y: lhs.origin.y - rhs.dy, width: lhs.width, height: lhs.height)
+    }
+
+    static public func + (lhs: CGRect, rhs: CGVector) -> CGRect {
+        return CGRect(x: lhs.origin.x + rhs.dx, y: lhs.origin.y + rhs.dy, width: lhs.width, height: lhs.height)
+    }
+
+    // MARK: - Expand
+
+    static public func - (lhs: CGRect, rhs: CGSize) -> CGRect {
+        return CGRect(x: lhs.origin.x, y: lhs.origin.y, width: lhs.width - rhs.width, height: lhs.height - rhs.height)
+    }
+
+    static public func + (lhs: CGRect, rhs: CGSize) -> CGRect {
+        return CGRect(x: lhs.origin.x, y: lhs.origin.y, width: lhs.width + rhs.width, height: lhs.height + rhs.height)
+    }
 }
