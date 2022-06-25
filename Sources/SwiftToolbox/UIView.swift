67ffc9cec8d416c62167ae6a3092c7e2d44a521b
Adam Wulf <adam.wulf@gmail.com>
2022-06-25 17:52:18 -0500

    Add safe mode option

--- Sources/SwiftToolbox/UIView.swift
+++ Sources/SwiftToolbox/UIView.swift
@@ -9,13 +9,20 @@
 import UIKit
 
 public extension UIView {
-    func layoutHuggingParent() {
+    func layoutHuggingParent(safeArea: Bool = false) {
         guard let superview = self.superview else { assertionFailure(); return }
         self.translatesAutoresizingMaskIntoConstraints = false
-        self.leadingAnchor.constraint(equalTo: superview.leadingAnchor).isActive = true
-        self.trailingAnchor.constraint(equalTo: superview.trailingAnchor).isActive = true
-        self.topAnchor.constraint(equalTo: superview.topAnchor).isActive = true
-        self.bottomAnchor.constraint(equalTo: superview.bottomAnchor).isActive = true
+        if #available(iOS 11.0, *), safeArea {
+            self.leadingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            self.trailingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            self.topAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.topAnchor).isActive = true
+            self.bottomAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        } else {
+            self.leadingAnchor.constraint(equalTo: superview.leadingAnchor).isActive = true
+            self.trailingAnchor.constraint(equalTo: superview.trailingAnchor).isActive = true
+            self.topAnchor.constraint(equalTo: superview.topAnchor).isActive = true
+            self.bottomAnchor.constraint(equalTo: superview.bottomAnchor).isActive = true
+        }
     }
 }
 #endif
