3ff40784561b0442d1b0ea98c754878a9911178c
Adam Wulf <adam.wulf@gmail.com>
2022-07-10 18:25:56 -0500

    Adding build script for documentation

---
+++ builddocs.sh
@@ -0,0 +1,1 @@
+swift package  --allow-writing-to-directory ./docs  generate-documentation  --target SwiftToolbox --output-path ./docs  --transform-for-static-hosting  --hosting-base-path swifttoolbox
