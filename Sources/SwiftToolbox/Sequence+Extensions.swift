85cdac03a3d6a845a221af7431eac0d6e017e22e
Adam Wulf <adam.wulf@gmail.com>
2022-07-31 00:36:28 -0500

    Added compactMap that also sends the index of the element

---
+++ Sources/SwiftToolbox/Sequence+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Adam Wulf on 7/31/22.
+//
+
+import Foundation
+
+extension Sequence {
+    public func compactMap<ElementOfResult>(_ transform: (Self.Element, Int) throws -> ElementOfResult?) rethrows -> [ElementOfResult] {
+        var ret: [ElementOfResult] = []
+        var index = 0
+        for item in self {
+            if let result = try transform(item, index) {
+                ret.append(result)
+            }
+            index += 1
+        }
+        return ret
+    }
+}
