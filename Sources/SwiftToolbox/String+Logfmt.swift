18014f5913fee95ff57e2cd38c1edc178d078d2c
Adam Wulf <adam.wulf@gmail.com>
2022-12-04 14:39:12 -0600

    reorder for clarity

--- Sources/SwiftToolbox/String+Logfmt.swift
+++ Sources/SwiftToolbox/String+Logfmt.swift
@@ -40,6 +40,10 @@ private extension String {
     @available(iOS 13.0, macOS 10.15, *)
     static private func logfmt(_ object: Any, attribute: String) -> String {
         switch object {
+        case let object as String:
+            return attribute.eqValue(object.contains("\"", " ") ? "\"\(object.slashEscaping("\""))\"" : object)
+        case let object as any Numeric:
+            return attribute.eqValue("\(object)")
         case let object as [String: Any]:
             return object.sorted(by: { $0.key < $1.key }).map({ keyVal in
                 return logfmt(keyVal.value, attribute: attribute.dot(keyVal.key))
@@ -49,10 +53,6 @@ private extension String {
                 return logfmt(item, attribute: attribute.dot("\(index)"))
             })
             return mapped.joined(separator: " ")
-        case let object as String:
-            return attribute.eqValue(object.contains("\"", " ") ? "\"\(object.slashEscaping("\""))\"" : object)
-        case let object as any Numeric:
-            return attribute.eqValue("\(object)")
         case let object as CustomLogfmtStringConvertible:
             return logfmt(object.logfmtDescription, attribute: attribute)
         case let object as CustomStringConvertible:
