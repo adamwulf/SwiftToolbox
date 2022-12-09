a9442c7c0158e3d1c18d3eac22f2214961aaf131
Adam Wulf <adam.wulf@gmail.com>
2022-12-08 21:18:18 -0600

    Adjusting documentation for PropertyListCompatible

--- Sources/SwiftToolbox/PropertyListCompatible.swift
+++ Sources/SwiftToolbox/PropertyListCompatible.swift
@@ -9,6 +9,11 @@ import Foundation
 import CoreGraphics
 
 /// An empty protocol implemented only by types that are safe to encode into a PList using `PropertyListSerialization`
+///
+/// The types that implement `PropertyListCompatible` are:
+/// `Int`, `Int64`, `Int32`, `Int16`, `Int8`, `UInt`, `UInt64`, `UInt32`, `UInt16`, `UInt8`, `CGFloat`,
+/// `Float`, `Double`, `String`, `Date`, `Data`, `NSDate`, `NSData`, `[PropertyListCompatible]`,
+/// and `[String: PropertyListCompatible]`
 public protocol PropertyListCompatible {
 }
 
@@ -16,6 +21,30 @@ extension Int: PropertyListCompatible {
 }
 
 extension Int64: PropertyListCompatible {
+}
+
+extension Int32: PropertyListCompatible {
+}
+
+extension Int16: PropertyListCompatible {
+}
+
+extension Int8: PropertyListCompatible {
+}
+
+extension UInt: PropertyListCompatible {
+}
+
+extension UInt64: PropertyListCompatible {
+}
+
+extension UInt32: PropertyListCompatible {
+}
+
+extension UInt16: PropertyListCompatible {
+}
+
+extension UInt8: PropertyListCompatible {
 }
 
 extension CGFloat: PropertyListCompatible {
