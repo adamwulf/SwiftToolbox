dcf2b48b138571741c9acc013b2cc8b8e35c624c
Adam Wulf <adam.wulf@gmail.com>
2023-01-17 22:33:56 -0600

    Adding extended types documentation https://forums.swift.org/t/enablement-of-docc-extension-support-as-an-opt-in-feature/62614

--- builddocs.sh
+++ builddocs.sh
@@ -1,6 +1,14 @@
-xcodebuild docbuild -scheme SwiftToolbox -destination generic/platform=iOS DOCC_EXEC=/Users/adamwulf/Developer/swift/swift-docc/.build/arm64-apple-macosx/debug/docc OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path SwiftToolbox --output-path docs"
-xcodebuild docbuild -scheme SwiftToolbox -destination generic/platform=iOS DOCC_EXEC=/Users/adamwulf/Developer/swift/swift-docc/.build/arm64-apple-macosx/debug/docc OTHER_DOCC_FLAGS="--output-path .build/SwiftToolbox.doccarchive"
+# building with default toolchain
+#
+# xcodebuild docbuild -scheme SwiftToolbox -destination generic/platform=iOS DOCC_EXEC=/Users/adamwulf/Developer/swift/swift-docc/.build/arm64-apple-macosx/debug/docc OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path SwiftToolbox --output-path docs"
+# xcodebuild docbuild -scheme SwiftToolbox -destination generic/platform=iOS DOCC_EXEC=/Users/adamwulf/Developer/swift/swift-docc/.build/arm64-apple-macosx/debug/docc OTHER_DOCC_FLAGS="--output-path .build/SwiftToolbox.doccarchive"
+
+# building with extended types docc
+# https://forums.swift.org/t/enablement-of-docc-extension-support-as-an-opt-in-feature/62614
+#
+xcrun -toolchain org.swift.57202301161a swift package --disable-sandbox generate-documentation --include-extended-types --disable-indexing --transform-for-static-hosting --hosting-base-path SwiftToolbox --output-path docs
+xcrun -toolchain org.swift.57202301161a swift package --disable-sandbox generate-documentation --include-extended-types --output-path .build/SwiftToolbox.doccarchive
 # format all json files in the Docs folder so that the built files are deterministic
-find Docs -name *.json -exec bash -c 'jq -M -c --sort-keys . < "{}" > "{}.temp"; mv "{}.temp" "{}"' \;
+find docs -name *.json -exec bash -c 'jq -M -c --sort-keys . < "{}" > "{}.temp"; mv "{}.temp" "{}"' \;
 # open the documentation
 open .build/SwiftToolbox.doccarchive
