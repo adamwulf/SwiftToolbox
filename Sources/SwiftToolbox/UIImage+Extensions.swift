2bc4470dc792e829e3d941000a4d8933719ff296
Adam Wulf <adam.wulf@gmail.com>
2023-02-04 15:28:22 -0600

    Updating docs

--- Sources/SwiftToolbox/UIImage+Extensions.swift
+++ Sources/SwiftToolbox/UIImage+Extensions.swift
@@ -5,9 +5,13 @@
 //  Created by Adam Wulf on 1/14/23.
 //
 
+#if canImport(UIKit)
 import UIKit
 
 public extension UIImage {
+    /// Crops the image to the input rect
+    /// - parameter rect: The portion of the image to crop to
+    /// - returns: The cropped image
     func cropped(to rect: CGRect) -> UIImage? {
         let rect = CGRect(x: rect.origin.x * scale, y: rect.origin.y * scale, width: rect.size.width * scale, height: rect.size.height * scale)
         guard let cgImage = cgImage?.cropping(to: rect) else {
@@ -16,3 +20,4 @@ public extension UIImage {
         return UIImage(cgImage: cgImage)
     }
 }
+#endif
