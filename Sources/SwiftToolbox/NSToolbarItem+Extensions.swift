2b6f508f8cd8f67b1ddf8db93a5b5321c3d4df3d
Adam Wulf <adam.wulf@gmail.com>
2023-02-04 15:47:00 -0600

    adding NSToolbarItem target/action method

---
+++ Sources/SwiftToolbox/NSToolbarItem+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Adam Wulf on 2/4/23.
+//
+
+import AppKit
+
+public extension NSToolbarItem {
+    /// Set both the target and action for the toolbar item
+    /// - parameter target: The ``target`` for the item
+    /// - parameter action: The ``action`` for the item
+    func set(target: AnyObject, action: Selector) {
+        self.target = target
+        self.action = action
+    }
+}
