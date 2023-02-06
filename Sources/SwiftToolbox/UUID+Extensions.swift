7c6ba27f84a386b680ba103a9bf6a28d5efb388d
Adam Wulf <adam.wulf@gmail.com>
2023-02-05 22:31:39 -0600

    Simple UUID

---
+++ Sources/SwiftToolbox/UUID+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Adam Wulf on 2/5/23.
+//
+
+import Foundation
+
+extension UUID {
+    /// Creates a simple UUID string consisting only of lowercase hex characters without `-`
+    static func simple() -> String {
+        return UUID().uuidString.lowercased().replacingCharacters(in: "-", with: "")
+    }
+}
