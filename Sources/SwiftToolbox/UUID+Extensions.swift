f5a8a44ec85e74a04ae7b6fa9846995f62935fcd
Adam Wulf <adam.wulf@gmail.com>
2023-02-05 22:32:55 -0600

    fixed error. built docs

--- Sources/SwiftToolbox/UUID+Extensions.swift
+++ Sources/SwiftToolbox/UUID+Extensions.swift
@@ -10,6 +10,6 @@ import Foundation
 extension UUID {
     /// Creates a simple UUID string consisting only of lowercase hex characters without `-`
     static func simple() -> String {
-        return UUID().uuidString.lowercased().replacingCharacters(in: "-", with: "")
+        return UUID().uuidString.lowercased().replacingOccurrences(of: "-", with: "")
     }
 }
