64ec8bec190c5a5a06bd6e4f95cdff172c106b0b
Adam Wulf <adam.wulf@gmail.com>
2023-05-10 13:53:04 -0500

    dictionary extensions

--- Sources/SwiftToolbox/Dictionary+Extensions.swift
+++ Sources/SwiftToolbox/Dictionary+Extensions.swift
@@ -9,9 +9,41 @@ import Foundation
 
 public extension Dictionary {
     /// Merges the input dictionary with this dictionary, preferring existing values in the case of a key conflict
+    ///
+    /// - Parameters:
+    ///   - other: The dictionary to merge with this dictionary
+    /// - Returns: A new dictionary with the merged values
     func merging(_ other: [Key: Value]) -> [Key: Value] {
         return merging(other, uniquingKeysWith: { val1, _ in
             return val1
         })
+    }
+
+    /// Returns a new dictionary with the same keys as this dictionary, but with each value transformed by the given closure.
+    ///
+    /// - Parameters:
+    ///   - transform: A closure that takes a key-value pair of this dictionary as its argument and returns a transformed value.
+    /// - Returns: A new dictionary with the same keys as this dictionary, but with each value transformed by the given closure.
+    func mapValues<T>(_ transform: (Key, Value) throws -> T) rethrows -> [Key: T] {
+        var result: [Key: T] = [:]
+        for (key, value) in self {
+            result[key] = try transform(key, value)
+        }
+        return result
+    }
+
+    /// Returns a new dictionary with the same keys as this dictionary, but with each value that is not nil transformed by the given closure.
+    ///
+    /// - Parameters:
+    ///   - transform: A closure that takes a key-value pair of this dictionary as its argument and returns an optional transformed value.
+    /// - Returns: A new dictionary with the same keys as this dictionary, but with each value that is not nil transformed by the given closure.
+    func compactMapValues<T>(_ transform: (Key, Value) throws -> T?) rethrows -> [Key: T] {
+        var result: [Key: T] = [:]
+        for (key, value) in self {
+            if let transformedValue = try transform(key, value) {
+                result[key] = transformedValue
+            }
+        }
+        return result
     }
 }
