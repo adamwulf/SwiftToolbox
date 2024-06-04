dd1b861d209a853d62b5b335ca33e5c191bc4053
Adam Wulf <adam@adamwulf.me>
2024-06-04 14:44:51 -0500

    Fix build on mac

--- Sources/SwiftToolbox/CGRect+Extensions.swift
+++ Sources/SwiftToolbox/CGRect+Extensions.swift
@@ -13,7 +13,7 @@ extension CGRect {
     /// A subscript that returns a `CGPoint` within the rectangle based on a `UnitPoint`.
     /// - Parameter unitPoint: A `UnitPoint` representing a relative position within the rectangle.
     /// - Returns: A `CGPoint` corresponding to the position within the rectangle.
-    @available(iOS 13.0, *)
+    @available(iOS 13.0, macOS 10.15, *)
     subscript(_ unitPoint: UnitPoint) -> CGPoint {
         return CGPoint(x: minX + unitPoint.x * width,
                        y: minY + unitPoint.y * height)
