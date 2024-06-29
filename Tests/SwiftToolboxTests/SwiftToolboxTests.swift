b3e1ba228a65a6f209078c5689b49c74a0ce04cb
Adam Wulf <adam@adamwulf.me>
2024-06-29 01:11:43 -0500

    Removing String.logfmt() in favor of Logfmt package

--- Tests/SwiftToolboxTests/SwiftToolboxTests.swift
+++ Tests/SwiftToolboxTests/SwiftToolboxTests.swift
@@ -396,56 +396,6 @@ final class SwiftToolboxTests: XCTestCase {
         XCTAssertEqual(string4, "")
     }
 
-    @available(iOS 13.0, macOS 10.15, *)
-    func testLogfmt() {
-        let str1 = "asdf"
-        XCTAssertEqual(String.logfmt(str1), "asdf")
-        let str2 = "asdf\"asdf\""
-        XCTAssertEqual(String.logfmt(str2), "\"asdf\\\"asdf\\\"\"")
-        let num = 12
-        XCTAssertEqual(String.logfmt(num), "12")
-        let debugObj = Fumble()
-        XCTAssertEqual(String.logfmt(debugObj), "debugStr")
-        let descObj = Mumble()
-        XCTAssertEqual(String.logfmt(descObj), "customString")
-        let logfmtObj = Bumble()
-        XCTAssertEqual(String.logfmt(logfmtObj), "grumblebumble")
-        let arr1 = ["asdf", "qwer"]
-        XCTAssertEqual(String.logfmt(arr1), "0=asdf 1=qwer")
-        let arr2 = ["asdf", "qwer thjfdg"]
-        XCTAssertEqual(String.logfmt(arr2), "0=asdf 1=\"qwer thjfdg\"")
-        let dict1 = ["asdf": "qwer thjfdg"]
-        XCTAssertEqual(String.logfmt(dict1), "asdf=\"qwer thjfdg\"")
-        let dict2 = ["asdf": ["qwer thjfdg"]]
-        XCTAssertEqual(String.logfmt(dict2), "asdf.0=\"qwer thjfdg\"")
-        let dict3 = ["asdf": ["fumble": "qwer thjfdg"]]
-        XCTAssertEqual(String.logfmt(dict3), "asdf.fumble=\"qwer thjfdg\"")
-        let dict4 = ["asdf": ["fumble", "mumble"]]
-        XCTAssertEqual(String.logfmt(dict4), "asdf.0=fumble asdf.1=mumble")
-        let optional1: Grumble? = Grumble()
-        let optional2: String? = nil
-        let dict5: [String: [Any?]] = ["asdf": [optional1, optional2]]
-        XCTAssertEqual(String.logfmt(dict5), "asdf.0=SwiftToolboxTests.Grumble asdf.1=[none]")
-        let optional3: String? = "grumble"
-        let optional4: String? = nil
-        let dict6: [String: [Any?]] = ["asdf": [optional3, optional4]]
-        XCTAssertEqual(String.logfmt(dict6), "asdf.0=grumble asdf.1=[none]")
-
-        let memoryContext = ["memory":
-                                ["current": ["footprint": 128,
-                                             "available": 2000 - 128,
-                                             "limit": 2000],
-                                 "peak": ["footprint": 348,
-                                          "available": 2000 - 348,
-                                          "limit": 2000]]]
-        XCTAssertEqual(String.logfmt(memoryContext), ["memory.current.available=1872",
-                                                      "memory.current.footprint=128",
-                                                      "memory.current.limit=2000",
-                                                      "memory.peak.available=1652",
-                                                      "memory.peak.footprint=348",
-                                                      "memory.peak.limit=2000"].joined(separator: " "))
-    }
-
     func testContainsCharacters() {
         let str = "hello world"
 
@@ -591,11 +541,6 @@ class Fumble: CustomDebugStringConvertible {
 class Mumble: CustomStringConvertible {
     var description: String {
         return "customString"
-    }
-}
-class Bumble: CustomLogfmtStringConvertible {
-    var logfmtDescription: String {
-        return "grumblebumble"
     }
 }
 class Grumble { }
